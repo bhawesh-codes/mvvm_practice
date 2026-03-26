627d5dbd9257208f433ff3cc9f051f38d279a024
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-26 18:06:52 +0545

    feat: Implement ApiService class for fetching new release books

---
+++ lib/data/services/api_services.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mvvm_practice/data/models/book_model.dart';
+
+class ApiService {
+  int page = 1;
+  Future<BookModel?> fetchBooks() async {
+  final response = await http.get(
+    Uri.parse('https://tbe.thuprai.com/v1/books/new-releases/?page=$page'),
+  );
+
+  if (response.statusCode == 200) {
+    
+    final bookModel = bookModelFromJson(response.body);
+
+    return bookModel;
+  } else {
+    debugPrint("Error: ${response.statusCode}");
+    return null;
+  }
+}
+  
+    
+
+  }
+
