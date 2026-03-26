84c776bcedcc967ee80fee281b93bff755864e00
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-26 18:14:00 +0545

    feat: Add main application structure

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_practice/view/screens/login_screen.dart';
+import 'package:mvvm_practice/viewmodel/home_viewmodel.dart';
+import 'package:mvvm_practice/viewmodel/login_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LoginViewModel()),
+        ChangeNotifierProvider(create: (_)=> HomeViewModel()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
+      ),
+    );
+  }
+}
