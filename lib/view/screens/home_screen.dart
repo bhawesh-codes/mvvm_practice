3c2879ce540a22d9611cfa506cfd63b4fbc7fcf7
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-27 14:24:15 +0545

    feat: Add logout button and next page button

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_practice/view/screens/login_screen.dart';
 import 'package:mvvm_practice/viewmodel/home_viewmodel.dart';
 import 'package:provider/provider.dart';
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,7 +14,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(() {
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<HomeViewModel>().fetchBooks();
     });
   }
@@ -29,7 +30,16 @@ class _HomeScreenState extends State<HomeScreen> {
         actions: [
           Padding(
             padding: EdgeInsets.only(right: 16),
-            child: Icon(Icons.person),
+            child: IconButton(
+              icon: Icon(Icons.person),
+              onPressed: () {
+                vm.logout();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => LoginScreen()),
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -51,44 +61,71 @@ class _HomeScreenState extends State<HomeScreen> {
 
           return Padding(
             padding: const EdgeInsets.all(8.0),
-            child: GridView.builder(
-              itemCount: books.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                childAspectRatio: 0.6,
-              ),
-              itemBuilder: (context, index) {
-                final book = books[index];
-
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(),
+            child: ListView(
+              children: [
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: books.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    childAspectRatio: 0.6,
+                  ),
+                  itemBuilder: (context, index) {
+                    final book = books[index];
+                
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(border: Border.all()),
+                            child: book.frontCover != null
+                                ? Image.network(
+                                    book.frontCover!,
+                                    fit: BoxFit.cover,
+                                    width: double.infinity,
+                                  )
+                                : Container(color: Colors.grey),
+                          ),
                         ),
-                        child: book.frontCover != null
-                            ? Image.network(
-                                book.frontCover!,
-                                fit: BoxFit.cover,
-                                width: double.infinity,
-                              )
-                            : Container(color: Colors.grey),
+                        SizedBox(height: 6),
+                        Text(
+                          book.title ?? "No title",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+
+                Center(
+                  child: Row(
+                    mainAxisAlignment: .center,
+                    children: [
+                      TextButton.icon(
+                        onPressed: () {
+                          context.read<HomeViewModel>().previousPage();
+                        },
+                        label: Text("Previous Page"),
+                        icon: Icon(Icons.arrow_back),
                       ),
-                    ),
-                    SizedBox(height: 6),
-                    Text(
-                      book.title ?? "No title",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                );
-              },
+                      SizedBox(width: 8),
+                      TextButton.icon(
+                        onPressed: () {
+                          context.read<HomeViewModel>().nextPage();
+                        },
+                        label: Text("Next Page"),
+                        icon: Icon(Icons.arrow_forward),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           );
         },
