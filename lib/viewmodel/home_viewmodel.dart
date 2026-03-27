48eb1d3f1a6ce740f884cb9bb3e1f97ba5c847a5
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-27 14:28:01 +0545

    feat: Add logout and next page logic

--- lib/viewmodel/home_viewmodel.dart
+++ lib/viewmodel/home_viewmodel.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_practice/data/models/book_model.dart';
 import 'package:mvvm_practice/data/services/api_services.dart';
+import 'package:mvvm_practice/data/services/secure_storage_service.dart';
 
 class HomeViewModel extends ChangeNotifier {
   final ApiService _apiService = ApiService();
+  final SecureStorageService _storage = SecureStorageService();
 
   BookModel? bookData;
   bool isLoading = false;
@@ -23,5 +25,24 @@ class HomeViewModel extends ChangeNotifier {
 
     isLoading = false;
     notifyListeners();
+  }
+  void logout(){
+    _storage.deleteToken();
+    bookData = null;
+    _apiService.page= 1;
+    notifyListeners();
+    
+  }
+  void nextPage(){
+    _apiService.page++;
+    fetchBooks();
+    notifyListeners();
+  }
+  void previousPage() {
+    if(_apiService.page > 1){
+    _apiService.page--;
+    fetchBooks();
+    notifyListeners();
+    }
   }
 }
