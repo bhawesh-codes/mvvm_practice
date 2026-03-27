57254ad42f4c2ee574e5d3b20edd74387a70a56e
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-27 16:42:16 +0545

    feat: Add authorization check to get book data

--- lib/data/services/api_services.dart
+++ lib/data/services/api_services.dart
@@ -3,27 +3,37 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:mvvm_practice/data/models/book_model.dart';
+import 'package:mvvm_practice/data/services/secure_storage_service.dart';
 
 class ApiService {
+  final SecureStorageService storage = SecureStorageService();
+  
   int page = 1;
   Future<BookModel?> fetchBooks() async {
-  final response = await http.get(
-    Uri.parse('https://tbe.thuprai.com/v1/books/new-releases/?page=$page'),
-  );
+    final String? token = await storage.getToken();
+    try {
+      final response = await http.get(
+        Uri.parse('https://tbe.thuprai.com/v1/books/new-releases/?page=$page'), 
+        headers: {'Authorization': 'Token $token'},
+      );
 
-  if (response.statusCode == 200) {
-    
-    final bookModel = bookModelFromJson(response.body);
-
-    return bookModel;
-  } else {
-    debugPrint("Error: ${response.statusCode}");
-    return null;
+      if (response.statusCode == 200) {
+        final bookModel = bookModelFromJson(response.body);
+        return bookModel;
+      } else {
+        debugPrint("Error: ${response.statusCode}");
+        return null;
+      }
+    } catch (e) {
+      debugPrint("Exception: $e");
+      return null;
+    }
+  } 
   }
-  
-}
+
+
   
     
 
-  }
+  
 
