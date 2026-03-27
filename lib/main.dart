8a1a6373ac3a2e9b266b4762d9901233368915c0
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-27 16:12:22 +0545

    feat: Add splash_screen for landing page logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm_practice/view/screens/login_screen.dart';
+import 'package:mvvm_practice/view/screens/splash_screen.dart';
 import 'package:mvvm_practice/viewmodel/home_viewmodel.dart';
 import 'package:mvvm_practice/viewmodel/login_viewmodel.dart';
 import 'package:provider/provider.dart';
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(body: LoginScreen()),
+        home: Scaffold(body: SplashScreen()),
       ),
     );
   }
