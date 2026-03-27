93b0742f108018a92b63931b282571516066331f
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-27 16:49:51 +0545

    refactor: optimize by disposing controller

--- lib/view/screens/login_screen.dart
+++ lib/view/screens/login_screen.dart
@@ -17,6 +17,13 @@ class _LoginScreenState extends State<LoginScreen> {
   final formKey = GlobalKey<FormState>();
 
   @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final lv = context.watch<LoginViewModel>();
     return Scaffold(
