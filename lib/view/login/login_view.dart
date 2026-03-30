e6fd51bae4bc10bd37296b14730058be54bea93b
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-30 13:06:14 +0545

    refactor: Convert LoginScreen from Stateful to Stateless and integrate ViewModel

--- lib/view/login/login_view.dart
+++ lib/view/login/login_view.dart
@@ -4,151 +4,106 @@ import 'package:mvvm_practice/view/home/home_view.dart';
 import 'package:mvvm_practice/view/login/login_viewmodel.dart';
 import 'package:provider/provider.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
-  final formKey = GlobalKey<FormState>();
-
-  @override
-  void dispose() {
-    emailController.dispose();
-    passwordController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final lv = context.watch<LoginViewModel>();
+    final vm = context.watch<LoginViewModel>();
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
+              const Text(
                 'Login',
                 style: TextStyle(fontSize: 36, fontWeight: FontWeight.w600),
               ),
-              SizedBox(height: 12),
-              Text('Hi, welcome back! Please login to your account.'),
-              SizedBox(height: 30),
+              const SizedBox(height: 12),
+              const Text('Hi, welcome back! Please login to your account.'),
+              const SizedBox(height: 30),
+
               SizedBox(
                 height: 340,
                 width: double.infinity,
                 child: Card(
                   child: Padding(
                     padding: const EdgeInsets.fromLTRB(8, 16, 8, 24),
-                    child: Center(
-                      child: Form(
-                        key: formKey,
-                        child: Column(
-                          mainAxisSize: .min,
-                          crossAxisAlignment: .start,
-                          children: [
-                            Text('Email'),
-                            SizedBox(height: 5),
-                            TextFormField(
-                              validator: Validators.emailValidator,
-                              controller: emailController,
-                              keyboardType: TextInputType.emailAddress,
-                              decoration: InputDecoration(
-                                hintText: 'Enter your Email..',
-                                border: OutlineInputBorder(
-                                  borderSide: BorderSide(),
-                                  borderRadius: BorderRadius.circular(16),
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 24),
-                            Text('Password'),
-                            SizedBox(height: 5),
-                            TextFormField(
-                              validator: Validators.passwordValidator,
-                              controller: passwordController,
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                hintText: 'Enter password',
-                                border: OutlineInputBorder(
-                                  borderSide: BorderSide(),
-                                  borderRadius: BorderRadius.circular(16),
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 30),
-                            Center(
-                              child: ElevatedButton(
-                                onPressed: () async {
-                                  if (formKey.currentState!.validate()) {
-                                    // Capture BEFORE the async gap
-                                    final navigator = Navigator.of(context);
-                                    final messenger = ScaffoldMessenger.of(
-                                      context,
-                                    );
+                    child: Form(
+                      key: vm.formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Email'),
+                          const SizedBox(height: 5),
 
-                                    await lv.login(
-                                      emailController.text,
-                                      passwordController.text,
-                                    );
+                          TextFormField(
+                            controller: vm.emailController,
+                            validator: Validators.emailValidator,
+                            keyboardType: TextInputType.emailAddress,
+                          ),
 
-                                    if (lv.token != null) {
-                                      messenger.showSnackBar(
-                                        SnackBar(
-                                          backgroundColor: Colors.green,
-                                          duration: Duration(milliseconds: 500),
-                                          content: Text(
-                                            "Login successful!",
-                                            style: TextStyle(
-                                              color: Colors.white,
+                          const SizedBox(height: 24),
+                          const Text('Password'),
+                          const SizedBox(height: 5),
+
+                          TextFormField(
+                            controller: vm.passwordController,
+                            validator: Validators.passwordValidator,
+                            obscureText: true,
+                          ),
+
+                          const SizedBox(height: 30),
+
+                          Center(
+                            child: ElevatedButton(
+                              onPressed: vm.isLoading
+                                  ? null
+                                  : () async {
+                                      final navigator = Navigator.of(context);
+                                      final messenger = ScaffoldMessenger.of(
+                                        context,
+                                      );
+
+                                      final success = await vm.login();
+
+                                      if (success) {
+                                        messenger.showSnackBar(
+                                          const SnackBar(
+                                            backgroundColor: Colors.green,
+                                            content: Text("Login successful!"),
+                                          ),
+                                        );
+
+                                        navigator.pushReplacement(
+                                          MaterialPageRoute(
+                                            builder: (_) => const HomeScreen(),
+                                          ),
+                                        );
+                                      } else {
+                                        messenger.showSnackBar(
+                                          SnackBar(
+                                            backgroundColor: Colors.red,
+                                            content: Text(
+                                              vm.error ??
+                                                  "Something went wrong",
                                             ),
                                           ),
-                                        ),
-                                      );
-                                      navigator.pushReplacement(
-                                        MaterialPageRoute(
-                                          builder: (_) => HomeScreen(),
-                                        ),
-                                      );
-                                    } else if (lv.error != null) {
-                                      messenger.showSnackBar(
-                                        SnackBar(
-                                          backgroundColor: Colors.red,
-                                          content: Text(
-                                            "Check your credentials!",
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                            ),
-                                          ),
-                                        ),
-                                      );
-                                    }
-                                  }
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  padding: EdgeInsets.all(8),
-                                  backgroundColor: Colors.blue,
-                                  minimumSize: Size(200, 50),
-                                ),
-                                child: lv.isLoading
-                                    ? CircularProgressIndicator(
-                                        color: Colors.white,
-                                      )
-                                    : Text(
-                                        'Login',
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                              ),
+                                        );
+                                      }
+                                    },
+                              child: vm.isLoading
+                                  ? const CircularProgressIndicator(
+                                      color: Colors.white,
+                                    )
+                                  : const Text('Login'),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
