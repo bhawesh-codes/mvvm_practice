16a98b66a17118dcfb40ab92aac3c964af774542
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-30 13:09:07 +0545

    refactor: Remove unnecessary import of flutter/widgets.dart in MyAppbar

--- lib/utils/const/my_appbar.dart
+++ lib/utils/const/my_appbar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mvvm_practice/view/favorite/favorite_view.dart';
 import 'package:mvvm_practice/view/home/home_viewmodel.dart';
