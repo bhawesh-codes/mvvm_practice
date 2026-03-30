18d49f638d32fbaca88bcbe2916a53a4bf5ab2b8
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-30 11:05:17 +0545

    feat: Add custom appbar

---
+++ lib/utils/const/my_appbar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mvvm_practice/view/favorite/favorite_view.dart';
+import 'package:mvvm_practice/view/home/home_viewmodel.dart';
+import 'package:mvvm_practice/view/login/login_view.dart';
+import 'package:provider/provider.dart';
+
+class MyAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppbar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      iconTheme: IconThemeData(color: Colors.white),
+      elevation: 5,
+      backgroundColor: Colors.blueAccent,
+      title: Text(
+        "Thuprai",
+        style: GoogleFonts.notoSans(
+          color: Colors.white,
+          fontSize: 24,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => FavoriteView()),
+            );
+          },
+          icon: Icon(Icons.favorite, color: Colors.white),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 16),
+          child: IconButton(
+            icon: Icon(Icons.exit_to_app, color: Colors.white),
+            onPressed: () {
+              context.read<HomeViewModel>().logout();
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (_) => LoginScreen()),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
