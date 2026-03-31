506c29f7b7b608c1525dc077cd1fb932b6149e1e
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-31 09:08:23 +0545

    refactor: Enhance HomeViewModel to manage favorite books and loading state

--- lib/view/home/home_viewmodel.dart
+++ lib/view/home/home_viewmodel.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mvvm_practice/services/api_services.dart';
 import 'package:mvvm_practice/services/secure_storage_service.dart';
 import 'package:mvvm_practice/view/home/models/book_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeViewModel extends ChangeNotifier {
   final ApiService _apiService = ApiService();
@@ -12,12 +13,19 @@ class HomeViewModel extends ChangeNotifier {
   bool isLoading = false;
   bool hasFetched = false; // ✅ add this
   String? error;
+  bool hasLoadedSlugs = false;
+
+  List<String> favoriteSlugs = [];
 
   Future<void> fetchBooks() async {
     if (isLoading) return;
     isLoading = true;
     hasFetched = true; // ✅ mark as fetched
     error = null;
+    if (!hasLoadedSlugs) {
+      await loadFavoriteSlugs();
+      hasLoadedSlugs = true;
+    }
     notifyListeners();
     try {
       final data = await _apiService.fetchBooks();
@@ -50,6 +58,36 @@ class HomeViewModel extends ChangeNotifier {
     bookData = null;
     hasFetched = false; // ✅ reset on logout
     _apiService.page = 1;
+    hasLoadedSlugs = false;
+    favoriteSlugs.clear();
     notifyListeners();
+  }
+
+  Future<void> loadFavoriteSlugs() async {
+    final pref = await SharedPreferences.getInstance();
+    favoriteSlugs = pref.getStringList(_favKey) ?? [];
+  }
+
+  static const String _favKey = 'favorite_slugs';
+
+  Future<void> toggleFavorite(Result book) async {
+    final pref = await SharedPreferences.getInstance();
+    // await pref.remove(  _favKey); // ✅ clear old data to prevent duplicates
+    final slugs = pref.getStringList(_favKey) ?? [];
+
+    if (favoriteSlugs.contains(book.slug)) {
+      favoriteSlugs.remove(book.slug); // ✅ remove by slug
+      slugs.remove(book.slug);
+    } else {
+      favoriteSlugs.add(book.slug!);
+      slugs.add(book.slug!); // ✅ save slug
+    }
+
+    await pref.setStringList(_favKey, slugs);
+    notifyListeners();
+  }
+
+  bool isFavorite(Result book) {
+    return favoriteSlugs.contains(book.slug); // ✅ compare by slug
   }
 }
