717c262fd0e0e45dfeec2b56181dd2ec43dba9d1
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-31 15:08:43 +0545

    refactor: remove unnecessary imports and global scoped providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mvvm_practice/repositories/favorites_repository.dart';
-import 'package:mvvm_practice/view/favorite/favorite_viewmodel.dart';
-import 'package:mvvm_practice/view/home/home_viewmodel.dart';
 import 'package:mvvm_practice/view/splash/splash_view.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,16 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final favRepo = FavoritesRepository();
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => HomeViewModel(favRepo)),
-        ChangeNotifierProvider(create: (_) => FavoriteViewModel(favRepo)),
-      ],
-      child: MaterialApp(
+    
+    return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(body: SplashScreen()),
-      ),
+        home: SplashScreen(
+
+            )
+        
     );
   }
 }
