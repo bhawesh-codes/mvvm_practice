e57cf25ae7367102129641860067d09b371fe355
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-31 09:09:20 +0545

    fix: Update FavoriteView to improve favorite book loading and toggle functionality

--- lib/view/favorite/favorite_view.dart
+++ lib/view/favorite/favorite_view.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_practice/utils/const/my_appbar.dart';
 import 'package:mvvm_practice/view/favorite/favorite_viewmodel.dart';
+import 'package:mvvm_practice/view/home/models/book_model.dart';
 import 'package:provider/provider.dart';
 
 class FavoriteView extends StatelessWidget {
-  const FavoriteView({super.key});
+  const FavoriteView({
+    super.key,
+    required this.toggleFavorite,
+    required this.isFavorite,
+  });
+
+  final Future<void> Function(Result) toggleFavorite;
+  final bool Function(Result) isFavorite;
 
   @override
   Widget build(BuildContext context) {
-    final vm = context.watch<FavoriteViewmodel>();
-    final books = vm.favoritebooks;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final vm = context.read<FavoriteViewModel>();
+      if (!vm.hasLoaded) {
+        // ✅ only fetch if not already fetched
+        vm.loadFavorites();
+      }
+    });
+    final fvm = context.watch<FavoriteViewModel>();
+
+    if (fvm.isLoading) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(color: Colors.blueAccent),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: MyAppbar(),
-      body: books.isEmpty
+      body: fvm.favoriteBooks.isEmpty
           ? const Center(child: Text("No favorite books"))
           : Padding(
               padding: const EdgeInsets.all(12),
@@ -20,16 +43,17 @@ class FavoriteView extends StatelessWidget {
                 children: [
                   GridView.builder(
                     shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: books.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      crossAxisSpacing: 8,
-                      mainAxisSpacing: 12,
-                      childAspectRatio: 0.6,
-                    ),
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: fvm.favoriteBooks.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 8,
+                          mainAxisSpacing: 12,
+                          childAspectRatio: 0.6,
+                        ),
                     itemBuilder: (context, index) {
-                      final book = books[index];
+                      final book = fvm.favoriteBooks[index];
 
                       return Card(
                         shape: RoundedRectangleBorder(
@@ -50,38 +74,48 @@ class FavoriteView extends StatelessWidget {
                                     : Container(color: Colors.grey),
                               ),
                             ),
-                            SizedBox(height: 6),
+                            const SizedBox(height: 6),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                SizedBox(width: 4),
+                                const SizedBox(width: 4),
                                 Expanded(
                                   child: Text(
                                     book.title ?? "No title",
                                     maxLines: 2,
                                     overflow: TextOverflow.ellipsis,
-                                    style: TextStyle(
+                                    style: const TextStyle(
                                       fontWeight: FontWeight.bold,
                                     ),
                                   ),
                                 ),
                                 IconButton(
-                                  onPressed: () {
-                                    context
-                                        .read<FavoriteViewmodel>()
-                                        .toggleFavorite(book);
+                                  onPressed: () async {
+                                    await toggleFavorite(
+                                      book,
+                                    ); // ✅ use passed function
+                                    if (context.mounted) {
+                                      // ✅ update list smoothly without loading
+                                      context
+                                          .read<FavoriteViewModel>()
+                                          .updateFavorites();
+                                    }
                                   },
-                                  icon: vm.isFavorite(book)
-                                      ? Icon(
+                                  icon:
+                                      isFavorite(book) // ✅ use passed function
+                                      ? const Icon(
                                           Icons.favorite,
                                           color: Colors.red,
                                           size: 20,
                                         )
-                                      : Icon(Icons.favorite_border, size: 20),
+                                      : const Icon(
+                                          Icons.favorite_border,
+                                          size: 20,
+                                        ),
                                 ),
                               ],
                             ),
-                            SizedBox(height: 6),
+                            const SizedBox(height: 6),
                           ],
                         ),
                       );
