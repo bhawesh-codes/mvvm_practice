49621d69a0d32206624e65a07526aea3435915af
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-31 11:17:13 +0545

    feat: Inject favoRepo into providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_practice/repositories/favorites_repository.dart';
 import 'package:mvvm_practice/view/favorite/favorite_viewmodel.dart';
-import 'package:mvvm_practice/view/splash/splash_view.dart';
 import 'package:mvvm_practice/view/home/home_viewmodel.dart';
+import 'package:mvvm_practice/view/splash/splash_view.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,11 +14,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final favRepo = FavoritesRepository();
     return MultiProvider(
       providers: [
-        
-        ChangeNotifierProvider(create: (_) => HomeViewModel()),
-        ChangeNotifierProvider(create: (_) => FavoriteViewModel()),
+        ChangeNotifierProvider(create: (_) => HomeViewModel(favRepo)),
+        ChangeNotifierProvider(create: (_) => FavoriteViewModel(favRepo)),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
