61d224b42dbd6992d91e9bddaae41301b87cbcc9
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-03-31 15:04:27 +0545

    refactor: fetchBooks  run immediately whenviewModel is created

--- lib/view/home/home_viewmodel.dart
+++ lib/view/home/home_viewmodel.dart
@@ -5,17 +5,20 @@ import 'package:mvvm_practice/repositories/favorites_repository.dart';
 import 'package:mvvm_practice/view/home/models/book_model.dart';
 
 class HomeViewModel extends ChangeNotifier {
-  final FavoritesRepository _favRepo;
-  HomeViewModel(this._favRepo);
+
   final ApiService _apiService = ApiService();
   final SecureStorageService _storage = SecureStorageService();
+  final favRepo = FavoritesRepository();
+  HomeViewModel() {
+    fetchBooks();
+  }
   
 
   ApiService get apiService => _apiService;
 
   BookModel? bookData;
   bool isLoading = false;
-  bool hasFetched = false;
+  
   String? error;
   bool hasLoadedSlugs = false;
   List<String> favoriteSlugs = [];
@@ -23,11 +26,10 @@ class HomeViewModel extends ChangeNotifier {
   Future<void> fetchBooks() async {
     if (isLoading) return;
     isLoading = true;
-    hasFetched = true;
     error = null;
 
     if (!hasLoadedSlugs) {
-      favoriteSlugs = await _favRepo.getSlugs(); // ✅ repo handles prefs
+      favoriteSlugs = await favRepo.getSlugs(); // ✅ repo handles prefs
       hasLoadedSlugs = true;
     }
 
@@ -44,7 +46,7 @@ class HomeViewModel extends ChangeNotifier {
   }
 
   Future<void> toggleFavorite(Result book) async {
-    await _favRepo.toggle(book); // ✅ repo handles logic
+    await favRepo.toggle(book); // ✅ repo handles logic
 
     // keep local slugs in sync
     if (favoriteSlugs.contains(book.slug)) {
@@ -57,18 +59,17 @@ class HomeViewModel extends ChangeNotifier {
   }
 
   bool isFavorite(Result book) {
-    return _favRepo.isFavorite(book, favoriteSlugs); // ✅ repo handles check
+    return favRepo.isFavorite(book, favoriteSlugs); // ✅ repo handles check
   }
 
   Future<void> refreshFavoriteSlugs() async {
-    favoriteSlugs = await _favRepo.getSlugs();
+    favoriteSlugs = await favRepo.getSlugs();
     notifyListeners();
   }
 
   void nextPage() {
     if (_apiService.page < 6) {
       _apiService.page++;
-      hasFetched = false;
       fetchBooks();
     }
   }
@@ -76,7 +77,6 @@ class HomeViewModel extends ChangeNotifier {
   void previousPage() {
     if (_apiService.page > 1) {
       _apiService.page--;
-      hasFetched = false;
       fetchBooks();
     }
   }
@@ -84,7 +84,6 @@ class HomeViewModel extends ChangeNotifier {
   void logout() {
     _storage.deleteToken();
     bookData = null;
-    hasFetched = false;
     _apiService.page = 1;
     hasLoadedSlugs = false;
     favoriteSlugs.clear();
