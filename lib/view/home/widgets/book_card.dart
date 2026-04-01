56b93dc9c77df16fb4a489525e43ef7e1a6d9371
Bhawesh <bhaweshbaruwal3@gmail.com>
2026-04-01 11:23:13 +0545

    feat: Add navigation to details view

--- lib/view/home/widgets/book_card.dart
+++ lib/view/home/widgets/book_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_practice/view/book_details/book_details_view.dart';
 import 'package:mvvm_practice/view/home/home_viewmodel.dart';
-import 'package:mvvm_practice/view/home/models/book_model.dart';
+import 'package:mvvm_practice/models/book_model.dart';
 import 'package:provider/provider.dart';
 
 class BookCard extends StatelessWidget {
@@ -12,47 +13,56 @@ class BookCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final vm = context.watch<HomeViewModel>();
 
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(border: Border.all()),
-              child: book.frontCover != null
-                  ? Image.network(
-                      book.frontCover!,
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                    )
-                  : Container(color: Colors.grey),
+    return GestureDetector(
+      onTap: () {
+        print("Navigating with slug: ${book.slug}");
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => BookDetailsView(slug: book.slug!)),
+        );
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(border: Border.all()),
+                child: book.frontCover != null
+                    ? Image.network(
+                        book.frontCover!,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                      )
+                    : Container(color: Colors.grey),
+              ),
             ),
-          ),
-          const SizedBox(height: 6),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const SizedBox(width: 4),
-              Expanded(
-                child: Text(
-                  book.title ?? "No title",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+            const SizedBox(height: 6),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(width: 4),
+                Expanded(
+                  child: Text(
+                    book.title ?? "No title",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              IconButton(
-                onPressed: () =>
-                    context.read<HomeViewModel>().toggleFavorite(book),
-                icon: vm.isFavorite(book)
-                    ? const Icon(Icons.favorite, color: Colors.red, size: 20)
-                    : const Icon(Icons.favorite_border, size: 20),
-              ),
-            ],
-          ),
-          const SizedBox(height: 6),
-        ],
+                IconButton(
+                  onPressed: () =>
+                      context.read<HomeViewModel>().toggleFavorite(book),
+                  icon: vm.isFavorite(book)
+                      ? const Icon(Icons.favorite, color: Colors.red, size: 20)
+                      : const Icon(Icons.favorite_border, size: 20),
+                ),
+              ],
+            ),
+            const SizedBox(height: 6),
+          ],
+        ),
       ),
     );
   }
